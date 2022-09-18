e72460dd8d2d8a7cbb2250b2ca4288ca823799de
snowii <meieralexander65@gmail.com>
2022-09-18 16:14:16 +0200

    Moved from GLFW to SDL

--- src/Vent_Window.cpp
+++ src/Vent_Window.cpp
@@ -2,63 +2,44 @@
 
 Vent_Window::Vent_Window(uint32_t width, uint32_t height, const char *title) : width(width), height(height)
 {
-	this->createWindow(title);
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		spdlog::critical("Error initializing SDL: {}", SDL_GetError());
+		exit(1);
+	}
+
+	handle = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+	if (!handle)
+	{
+		spdlog::critical("Error creating SDL window");
+		exit(1);
+	}
 }
 
 Vent_Window::~Vent_Window()
 {
-	glfwTerminate();
+	SDL_DestroyWindow(handle);
+	SDL_Quit();
 }
 
-void error_callback(int error, const char *description)
+bool Vent_Window::handleEvents()
 {
-	spdlog::error("GLFW Error (code {}): {}", error, description);
-}
-
-void Vent_Window::createWindow(const char *title)
-{
-#if defined(VK_USE_PLATFORM_XLIB_KHR)
-	glfwInitHint(GLFW_X11_XCB_VULKAN_SURFACE, false);
-#endif
-
-	if (!glfwInit())
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
 	{
-		spdlog::critical("Failed to Initialize GLFW");
-		exit(-1);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			return false;
+		}
 	}
-
-	glfwSetErrorCallback(error_callback);
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
-	handle = glfwCreateWindow(width, height, title, nullptr, nullptr);
-
-	if (!handle)
-	{
-		glfwTerminate();
-		spdlog::critical("Failed to create GLFW Window");
-		exit(-1);
-	}
-
-	glfwSetWindowUserPointer(handle, this);
-	glfwSetFramebufferSizeCallback(handle, framebufferResizeCallback);
-}
-
-void Vent_Window::update() const noexcept
-{
-	glfwPollEvents();
-}
-void Vent_Window::framebufferResizeCallback(GLFWwindow *window, int width, int height)
-{
-	auto vWindow = reinterpret_cast<Vent_Window *>(glfwGetWindowUserPointer(window));
-	vWindow->width = width;
-	vWindow->height = height;
+	return true;
 }
 
 vk::SurfaceKHR Vent_Window::createSurface(vk::Instance instance)
 {
 	VkSurfaceKHR _surface;
-	VkResult err = glfwCreateWindowSurface(static_cast<VkInstance>(instance), handle, nullptr, &_surface);
-	if (err != VK_SUCCESS)
+	if (!SDL_Vulkan_CreateSurface(handle, static_cast<VkInstance>(instance), &_surface))
 		throw std::runtime_error("Failed to create window surface!");
 	return static_cast<vk::SurfaceKHR>(_surface);
 }
